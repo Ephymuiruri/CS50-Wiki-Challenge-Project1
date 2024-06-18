90df6491031da8f481aff35a4ebf8627c065ce97
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-06-18 06:21:10 +0300

    adede render path for specific page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
+import markdown2
 from django.shortcuts import render
-
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def page(request,name):
+    markdown_content = util.get_entry(name)
+    if markdown_content:
+        html_content = markdown2.markdown(markdown_content)
+        return render(request, "encyclopedia/page.html", {
+            "title":name,
+            "html_content": html_content
+        })
+    else:
+        return render(request, "encyclopedia/error.html")
